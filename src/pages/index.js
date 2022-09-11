d3bc29e93262bb1dd3ab42d783f90e94233c087f
DInil Rubasinghe <dinilrube12@gmail.com>
2022-09-11 21:43:52 +0530

    resolve fontawesome 1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,5 @@
 import React from 'react'
 import Layouts from '../components/Layouts'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-  faInstagram,
-  faGithub,
-  faPinterest
-} from '@fortawesome/free-brands-svg-icons'
 
 export default function Home () {
   return (
@@ -14,11 +8,6 @@ export default function Home () {
         <div>
           <h1>Hi! I'm Dinil</h1>
           <p>Nice to meet you!✌️</p>
-          <div>
-            <a href='https://instagram.com/xdinil_h' target={'_blank'}>
-            <FontAwesomeIcon id='arrd' icon={faInstagram} size='3x' />
-            </a>
-          </div>
         </div>
         <div>
           <p>A passionate Student, an Explorer, a Programmer, a Great Designer and a Hobiest</p>
