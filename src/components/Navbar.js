123cf23d25a2179c766b35a7222dd226c32787f2
DInil Rubasinghe <dinilrube12@gmail.com>
2022-09-18 00:04:28 +0530

    18/09, new about design

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,18 @@
 import { Link } from 'gatsby'
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import * as styles from '../styles/Navbar.module.scss'
+import { faUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 
 export default function Navbar() {
   return (
     <nav>
       <Link to='/'>Home</Link>
-      <Link to='/about'>About</Link>
-      <a href='https://github.com/dinilrubasinghe' target={'_blank'}>Github</a>
+      <Link to='#about'>About</Link>
+      <a href='https://github.com/dinilrubasinghe' target={'_blank'}>
+        Github&nbsp;
+        <FontAwesomeIcon icon={faUpRightFromSquare} size='1x'/>
+        </a>
     </nav>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Layouts from '../components/Layouts'
+import { StaticImage } from 'gatsby-plugin-image'
 import * as styles from '../styles/Home.module.scss'
 
 export default function Home () {
@@ -12,7 +13,21 @@ export default function Home () {
           <p className={styles.sum}>A passionate Student, an Explorer, a Programmer, a Great Designer and a Hobiest</p>
         </div>
         <div className={styles.column}>
-
+          <div>
+          <StaticImage src='../images/Group 11.png' alt='DpBokeh' className={styles.bokeh}/>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className={styles.row}>
+          <div className={styles.column2}>
+            <div className={styles.knowMe}>
+              <p>Hi!👋 Let's get to know me</p>
+            </div>
+          </div>
+          <div className={styles.column2}>
+            <h1 className={styles.title}>About</h1>
+          </div>
         </div>
       </section>
     </Layouts>
